3d9394eacef60f2e9c57b3c2e40a3192c8815f18
drhb <habib.s.t.bukhari@gmail.com>
2023-04-21 23:39:42 +0000

    added ability to modify original `json`

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # icecube-2nd-place
 Code  for 2nd Place Icecube neutrino competition 
+
+
+```python
+python train.py config.json NUM_WORKERS 20 SEED 2023 MODEL_KWARGS.dim 38
+```
--- train.py
+++ train.py
@@ -12,51 +12,67 @@ from src.loss import loss, loss_vms
 from fastxtend.vision.all import EMACallback
 from tqdm import tqdm
 from src.utils import seed_everything, WrapperAdamW
-import config
+import argparse
+import json
+import torch
+import os
+import sys
+from src.models import (
+    DeepIceModel,
+    EncoderWithDirectionReconstructionV22,
+    EncoderWithDirectionReconstructionV23,
+)
+from pdb import set_trace
+
+
+def read_config_file(file_path):
+    with open(file_path, 'r') as f:
+        config_data = json.load(f)
+    return config_data
 
 
 def train(config):
     ds_train = IceCubeCache(
-        config.PATH,
+        config['PATH'],
         mode="train",
-        L=config.L,
-        selection=config.SELECTION,
+        L=config["L"],
+        selection=config["SELECTION"],
         reduce_size=0.125,
     )
     ds_train_len = IceCubeCache(
-        config.PATH,
+        config["PATH"],
         mode="train",
-        L=config.L,
+        L=config["L"],
         reduce_size=0.125,
-        selection=config.SELECTION,
+        selection=config["SELECTION"],
         mask_only=True,
     )
     sampler_train = RandomChunkSampler(ds_train_len, chunks=ds_train.chunks)
     len_sampler_train = LenMatchBatchSampler(
-        sampler_train, batch_size=config.BS, drop_last=True
+        sampler_train, batch_size=config["BS"], drop_last=True
     )
     dl_train = DeviceDataLoader(
         torch.utils.data.DataLoader(
             ds_train,
             batch_sampler=len_sampler_train,
-            num_workers=config.NUM_WORKERS,
+            num_workers=config["NUM_WORKERS"],
             persistent_workers=True,
         )
     )
 
     ds_val = IceCubeCache(
-        config.PATH, mode="eval", L=config.L_VALID, selection=config.SELECTION
+        config["PATH"], mode="eval", L=config["L_VALID"], selection=config["SELECTION"]
     )
     ds_val_len = IceCubeCache(
-        config.PATH,
+        config["PATH"],
         mode="eval",
-        L=config.L_VALID,
-        selection=config.SELECTION,
+        L=config["L_VALID"],
+        selection=config["SELECTION"],
         mask_only=True,
     )
     sampler_val = torch.utils.data.SequentialSampler(ds_val_len)
     len_sampler_val = LenMatchBatchSampler(
-        sampler_val, batch_size=config.BS_VALID, drop_last=False
+        sampler_val, batch_size=config["BS_VALID"], drop_last=False
     )
     dl_val = DeviceDataLoader(
         torch.utils.data.DataLoader(
@@ -65,35 +81,65 @@ def train(config):
     )
 
     data = DataLoaders(dl_train, dl_val)
-    model = config.MODEL(**config.MODEL_KWARGS)
-    if config.WEITHS:
-        print("Loading weights from ...", config.WEITHS)
-        model.load_state_dict(torch.load(config.WEITHS))
+    model = config.MODEL(**config["MODEL_KWARGS"])
+    if config["WEITHS"]:
+        print("Loading weights from ...", config["WEITHS"])
+        model.load_state_dict(torch.load(config["WEITHS"]))
     model = nn.DataParallel(model)
     model = model.cuda()
     learn = Learner(
         data,
         model,
-        path=config.OUT,
-        loss_func=config.LOSS_FUNC,
+        path=config["OUT"],
+        loss_func=config["LOSS_FUNC"],
         cbs=[
             GradientClip(3.0),
             CSVLogger(),
             SaveModelCallback(monitor="loss", comp=np.less, every_epoch=True),
-            GradientAccumulation(n_acc=4096 // config.BS),
+            GradientAccumulation(n_acc=4096 // config["BS"]),
         ],
-        metrics=[config.METRIC],
+        metrics=[config["METRIC"]],
         opt_func=partial(WrapperAdamW, eps=1e-7),
     ).to_fp16()
 
-
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--config_name", type=str, default=None)
+    parser = argparse.ArgumentParser(description='Create a model from a JSON config file.')
+    parser.add_argument('config_file', type=str, help='Path to the JSON config file.')
+    parser.add_argument('configs', nargs='*', metavar=('KEY', 'VALUE'),
+                        help='The JSON config key to override and its new value.')
+
     args = parser.parse_args()
-    configs = eval(f"config.{args.config_name}")
-    print(f"Training with config: {configs.__dict__}")
+    config_file_path = args.config_file
+
+    config_data = read_config_file(config_file_path)
+
+    if args.configs:
+        for config_key, config_value in zip(args.configs[::2], args.configs[1::2]):
+            keys = config_key.split('.')
+            last_key = keys.pop()
+
+            current_data = config_data
+            for key in keys:
+                current_data = current_data[key]
+
+            try:
+                value = json.loads(config_value)
+            except json.JSONDecodeError:
+                value = config_value
+
+            current_data[last_key] = value
+
+    print("Training with the following configuration:")
+    print(json.dumps(config_data, indent=4))
     print("_______________________________________________________")
-    seed_everything(configs.SEED)
-    os.makedirs(configs.OUT, exist_ok=True)
-    train(configs)
+
+    config_data["MODEL"] = getattr(sys.modules[__name__], config_data["MODEL"])
+    config_data["LOSS_FUNC"] = getattr(sys.modules[__name__], config_data["LOSS_FUNC"])
+    config_data["METRIC"] = getattr(sys.modules[__name__], config_data["METRIC"])
+
+    seed_everything(config_data["SEED"])
+    os.makedirs(config_data["OUT"], exist_ok=True)
+    #train(config_data)
+
+if __name__ == "__main__":
+    main()
